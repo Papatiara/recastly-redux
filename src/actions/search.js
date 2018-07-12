e7e9a3c3043d43997f3a8274c018868fb5bcc3e2
Papatiara <shirazipatricia@gmail.com>
2018-07-12 11:09:48 -0700

    All tests passing

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,8 +4,23 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
-var handleVideoSearch = (q) => {
- 
+var handleVideoSearch = (videos) => {
+
+return (dispatch) => {
+dispatch(changeVideo(true));
+fetch(videos)
+    .then((response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        dispatch(changeVideo(false));
+        return response;
+    })
+    .then((response) => response.json())
+    .then((items) => dispatch(changeVideoList(items)))
+    .catch(() => dispatch(searchYouTube({YOUTUBE_API_KEY, videos}, changeVideo)));
+  };
+
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
 import handleSearchChange from '../actions/search.js';
-var SearchContainer = () => {};
+
+var mapDispatchToProps = (dispatch) => ({
+  handleSearchInputChange: (video) => dispatch(handleSearchChange(video))
+});
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
 //HINT: use react-redux 'connect' method to generate a container component from
 //state and dispatch mappings.
+
+var SearchContainer = connect(null, mapDispatchToProps) (Search);
 
 export default SearchContainer;
